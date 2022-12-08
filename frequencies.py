70a53d2d2450240ca1d90bf7541ec8f6587b2f6b
Richmond <richmond.bobie@outlook.com>
2022-12-08 11:19:55 +0000

    Complete frequencies task

--- frequencies.py
+++ frequencies.py
@@ -3,7 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
+    str(items)
     for element in items:
         frequencies[element] = frequencies.get(element, 0) + 1
     return frequencies
+
 
